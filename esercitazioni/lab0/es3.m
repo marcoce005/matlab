a0a0077bd84b38887d72baeeeb03b3571305b1b1
marcoce005 <marcoce005@gmail.com>
2025-03-21 10:27:58 +0000

    update in lab0/es3

--- esercitazioni/lab0/es3.m
+++ esercitazioni/lab0/es3.m
@@ -5,6 +5,19 @@ A = [
     15 16 17 18 19 20
 ];
 
+%{
+
+oppure 
+
+A = [
+    1 : 6;
+    5 : 10;
+    9 : 14;
+    15 : 20
+];
+
+%}
+
 B = A(:, end : -1 : 1);       % costruire la matrice B formata dalle colonne di A disposte in ordine inverso (ossia, la prima colonna di B è la sesta di A, la seconda di B è la quinta di A...);
 
 even_row = A(2 : 2 : end, :);       % costruire la matrice formata dalle sole colonne pari di A
@@ -15,4 +28,4 @@ C = A([1 4 3], [5 2]);              % costruire la matrice formata dalle righe 1
 
 diagonal = diag(A)';                % costruire il vettore formato dagli elementi diagonali ak,k , k = 1, . . . , 4 di A.
 % oppure
-diagonal2 = A(1 : 5 : 5 * 4);
+diagonal2 = A(1 : 5 : 4 * 4);           % srotola per colonne
